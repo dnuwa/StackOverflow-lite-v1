12629f97215da767b6245ad26e6aeac539b2617f
daniel nuwa <daniel.nuwa@gmail.com>
2018-08-24 23:46:55 +0300

    created the platfrom database and established a connection [Finishes #160031866 #160029379]

---
+++ models/dbmodels.py
@@ -0,0 +1,34 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+class DatabaseAccess:
+    def __init__(self):
+        if os.getenv('APP_SETTINGS') == "testing":
+            self.dbname = "test_db"
+
+        else:
+            self.dbname = "stackoverflow-lite"
+
+        try: 
+            self.conn = psycopg2.connect(dbname="{}".format(self.dbname), user="postgres", password="1234567890", host="localhost")
+            self.conn.autocommit = True
+            
+            self.cursor = self.conn.cursor(cursor_factory=RealDictCursor)
+            print("database Connected")
+
+        except psycopg2.Error:
+            print("can not establish a database connection")
+
+    def dbtest(self):
+        print ("we got this")
+
+
+if __name__ == '__main__':
+    db = DatabaseAccess()
+    db.dbtest()
+    app.run(debug=True)
+            
