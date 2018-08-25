e272ea3d557da7a27dd8a0de62b0d61d6ec9b35d
daniel nuwa <daniel.nuwa@gmail.com>
2018-08-25 10:54:13 +0300

    added queries to create database tables [Delivers #160029379]

--- models/dbmodels.py
+++ models/dbmodels.py
@@ -23,8 +23,18 @@ class DatabaseAccess:
         except psycopg2.Error:
             print("can not establish a database connection")
 
-    # def dbtest(self):
-    #     print ("we got this")
+    def create_table_subscribers(self):
+        sql_query = "CREATE TABLE IF NOT EXISTS subscribers(user_id serial PRIMARY KEY, display_name varchar(100) NOT NULL, email varchar(100) NOT NULL, password varchar(100) NOT NULL"
+        self.cursor.execute(sql_query)
+        print ('table created')
+
+    def create_table_questions(self):
+        sql_query = "CREATE TABLE IF NOT EXISTS questions(qn_id serial PRIMARY KEY, user_id varchar(100) NOT NULL, question varchar(1000) NOT NULL"
+        self.cursor.execute(sql_query)
+
+    def create_table_answer(self):
+        sql_query = "CREATE TABLE IF NOT EXISTS answers(ans_id serial PRIMARY KEY, qn_id varchar(100) NOT NULL, user_id varchar(100) NOT NULL, answer varchar(200) NOT NULL, ans_state varchar(20)"
+        self.cursor.execute(sql_query)
 
 
 # if __name__ == '__main__':
